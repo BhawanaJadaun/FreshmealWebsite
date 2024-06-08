95994deaf8b516ff001e30b4de2924400974dbe0
BhawanaJadaun <bhawanajadaun0001@gmail.com>
2024-06-08 22:53:41 +0530

    Edit Something

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
 <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 place-items-center min-h-[600px]'>
 {/* Text-content-section */}
 <div className='space-y-7 text-dark order-2 sm:order-1'>
-<h1 data-aos = "fade-up" className='text-5xl'>Fresh & Healthy Meal PLan 
+<h1 data-aos = "fade-up" className='text-5xl'>Fresh & Healthy Meal Plan 
 <span className='text-secondary font-cursive text-7xl'>Delivery</span>{" "}
  in Miami</h1>
  <p data-aos = "fade-up" data-aos-delay = "500" className='lg:pr-64'>Delicious Meals Delivered to Your Door from $132.95 per week</p>
